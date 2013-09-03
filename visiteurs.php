d0b47eb38dd40e505f5de0f67f20b79cfe96ac17
oboucha <mastyknight@hotmail.com>
2013-09-03 11:19:46 -0400

    Code pour afficher un fichier texte

--- visiteurs.php
+++ visiteurs.php
@@ -1,4 +1,10 @@
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<script>
+    function popup()
+    {
+        alert("Vous utilisez Internet Explorer");
+    }
+</script>
 <body><?php
 /**
  * Created by JetBrains PhpStorm.
@@ -7,6 +13,10 @@
  * Time: 09:00
  * To change this template use File | Settings | File Templates.
  */
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
+    echo '<script>popup()</script>';
+}
+
 $fp = fopen("pays.txt","r");
 while(!feof($fp)) {
     $page = fgets($fp, 4096);
