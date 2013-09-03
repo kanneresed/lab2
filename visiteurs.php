1ad7f4b04d9366af21d3c15a4938a3c7f131bbe6
oboucha <mastyknight@hotmail.com>
2013-09-03 11:35:09 -0400

    Code pour afficher un fichier texte

--- visiteurs.php
+++ visiteurs.php
@@ -25,6 +25,6 @@ while(!feof($fp)) {
 }
 echo 'Voici les Pays: </br>' .$affiche;
 fclose($fp);
-
 ?>
+<img src="http://fc06.deviantart.net/fs70/f/2010/363/c/c/starcraft_ii_battlecruiser_by_xcreepingdeathx-d35xnar.jpg" width="854" height="480" >
 </body>
